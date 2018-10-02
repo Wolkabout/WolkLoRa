7491bbc6db60d911cf05c07bb180b36e7991fe35
srdjastankovic <srdjan.stankovic@wolkabout.comm>
2018-10-02 14:43:28 +0200

    Initial adding

---
+++ README.md
@@ -0,0 +1,64 @@
+# Intro
+--------
+WolkNode-TTN repo consists Arduino IDE example which makes connection to [The Things Network](https://www.thethingsnetwork.org/). It's based on [RN2483-Arduino-Library](https://github.com/jpmeijers/RN2483-Arduino-Library).
+Device measure enviroment temperature data on hearbeat event. This data can be routed from TTN to [WolkAbout IoT Platform](https://demo.wolkabout.com/#/login) using apropiated decoder(below described as [Application Decoder](#decoder)) and WolkAbout IoT Platform wizard.
+
+# Prerequest
+-----
+**Used Hardware:**
+
+ * [Arduino UNO](https://store.arduino.cc/usa/arduino-uno-rev3)
+ * [Mikroe LoRa click - RN2483](https://www.mikroe.com/lora-rf-click)
+ * [DS18B20 Temperature Sensor](https://eu.mouser.com/ProductDetail/Maxim-Integrated/DS18B20%2b?qs=sGAEpiMZZMvbyeSUH4qH%2fLbikZ7SIep9)
+
+**Firmware dependencies:**
+
+ * Install following Arduino libs([HOW TO](https://www.arduino.cc/en/guide/libraries)):
+   * OneWire
+   * DallasTemperature
+ 
+ * Include files:
+   * src/rn2xx3.cpp and src/rn2xx3.h with *Sketch/Include Library/Add .ZIP Library...* in Arduino IDE. Original source is [RN2483-Arduino-Library](https://github.com/jpmeijers/RN2483-Arduino-Library).
+
+# Example of usage
+------
+
+## Hardware Connection
+<center>
+![WolkTTN-Connection_-_Small](/uploads/888f122810f2381fdc2e7040a95ef584/WolkTTN-Connection_-_Small.png)
+</center>
+
+## Firmware Deployment
+
+* Open one of *WolkNode_TTN.ino* example
+* **#define** LoRA connection mode, **OTAA** or **ABP**
+* Insert your TTN device parameters. Copy *Example Code* from device overview into WolkNode_TTN.ino
+* Verify and Upload to device
+
+<a name="decoder">
+
+# Application Decoder
+
+-----
+</a>
+
+To route data from TTN to WolkAbout IoT Platform create device on WolkAbout IoT Platofrm and copy following decoder into ***Payload Formats*** section on *TTN Applications*. Note that name of methode *'.T'* over *var decoded* is *REF* from device manifest on WolkAbout IoT platform.
+
+```
+function Decoder(bytes, port) {
+  // Decode an uplink message from a buffer
+  // (array) of bytes to an object of fields.
+  var decoded = {};
+  var type = bytes[0];
+  
+  switch (type) {
+    case 1:
+      decoded.T = bytes[1] + bytes[2]/100;
+      break;
+    default:
+      decoded.T = bytes[0];
+  }
+  
+  return decoded;
+}
+```
---
+++ WolkTTN/WolkTTN.ino
@@ -0,0 +1,137 @@
+/*
+ * Author: sstankovic
+ * Date: 2018-03-06
+ * Filename: WolkNode-TTN
+ * Decription: Project core is based on https://github.com/jpmeijers/RN2483-Arduino-Library. 
+ *             This program is meant to be used with an Arduino UNO, conencted to any RN2483 radio module and DS1820 temperature sensor.
+ *             Don't forget to CHANGE CONNECTION PARAMETERS!
+ *
+ *     Copyright 2018 WolkAbout Technology s.r.o.
+ */
+#include <rn2xx3.h>
+#include <SoftwareSerial.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define debugSerial Serial
+
+#define OTAA                     //DEFINE CONNECTION MODE: ABP or OTAA
+#define HEARTBEAT 60000          //in ms
+#define BYTES_TO_SEND 3
+#define ONE_WIRE_BUS 2
+
+#ifdef OTAA                      // Copy from TTN Console/Device Overview your AppEUI and AppKey for OTAA mode
+const char *appEui = "70B3D57ED00096A0";
+const char *appKey = "DB3CB2B8D77C8114C2247090E2C9AEAD";
+#else //ABP                      // Copy from TTN Console/Device Overview your DevAddr, NwkSKey and AppSKey for ABP mode
+const char *devAddr = "260114E1";
+const char *nwkSKey = "8B452ACA0135B180CFC18ADAE8E06B51";
+const char *appSKey = "93BC936B1B2B5E8A351B3F6D62B56ED4";
+#endif
+bool join_result = false;
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+rn2xx3 myLora(mySerial);
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+
+void setup(){
+  mySerial.begin(9600);
+  debugSerial.begin(115200);
+  debugSerial.println("Startup");
+
+  initializeRadio();
+
+  initLed();
+  sensors.begin();
+  delay(2000);
+}
+
+void loop(){
+  setLed(HIGH);
+  debugSerial.print("\n\rRequesting temperatures...");
+  sensors.requestTemperatures();
+  debugSerial.print(" Temperature is: ");
+  float temperature = sensors.getTempCByIndex(0);
+  debugSerial.print(temperature);
+  debugSerial.print(" C \n\r");
+  int temperature_high = temperature;
+  int temperature_low = 100 * (temperature - temperature_high);
+
+  byte payload[3];
+  payload[0] = 0x01;
+  payload[1] = lowByte(temperature_high);
+  payload[2] = lowByte(temperature_low);
+
+  debugSerial.print(" Sending payload to TTN ");
+  join_result = myLora.tx(payload);
+  if(!join_result){
+    debugSerial.print("Failed to transmit to readings. \n\r Status is:");
+    debugSerial.print(join_result);
+    
+    while(!join_result){
+      debugSerial.println("...trying to reconnect to TTN Gateway. \n\rDo you have TTN coverage?");
+      delay(60000);              //delay a minute before retry
+      join_result = myLora.init();
+    }
+  }
+  debugSerial.print("Successfully");
+  
+  setLed(LOW);
+  myLora.sleep(HEARTBEAT);
+  delay(HEARTBEAT);
+}
+
+
+
+void initializeRadio(){
+  pinMode(12, OUTPUT);    //reset RN2483
+  digitalWrite(12, LOW);
+  delay(500);
+  digitalWrite(12, HIGH);
+
+  
+  delay(100);             //wait for the RN2xx3's startup message
+  mySerial.flush();
+
+  myLora.autobaud();     //Autobaud the rn2483 module to 9600. The default would otherwise be 57600.
+
+  String hweui = myLora.hweui();
+  while(hweui.length() != 16){
+    debugSerial.println("Communication with RN2xx3 unsuccessful. Power cycle the board.");
+    debugSerial.println(hweui);
+    delay(10000);
+    hweui = myLora.hweui();
+  }
+
+  debugSerial.println("When using OTAA, register this DevEUI: ");
+  debugSerial.println(myLora.hweui());
+  debugSerial.println("RN2xx3 firmware version:");
+  debugSerial.println(myLora.sysver());
+
+  debugSerial.println("Trying to join TTN");
+  #ifdef OTAA
+    debugSerial.println("-- OTAA SELECTED --\n\r-- JOIN OTAA --");
+    join_result = myLora.initOTAA(appEui, appKey);
+  #else
+    debugSerial.println("-- ABP SELECTED --\n\r-- PERSONALIZE ABP --");
+    join_result = myLora.initABP(devAddr, appSKey, nwkSKey);
+  #endif
+
+  while(!join_result){
+    debugSerial.println("Unable to join. Are your keys correct, and do you have TTN coverage?");
+    delay(60000);       //delay a minute before retry
+    join_result = myLora.init();
+  }
+  debugSerial.println("Successfully joined TTN");
+}
+
+void initLed(){
+    pinMode(4, OUTPUT);
+}
+
+void setLed(bool state){
+  digitalWrite(4, state);
+}
+
